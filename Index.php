0ac21ac5899a9af37e05c92e49e9cfe9770ed3df
Yizhiping <45778513+Yizhiping@users.noreply.github.com>
2019-06-05 17:09:27 +0800

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ping_yi
+ * Date: 2019/6/5
+ * Time: 14:48
+ */
+
+?>
+
+<html>
+<link href="Css/MainPage.css" rel="stylesheet">
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <title>成倉管理系統</title>
+</head>
+<body>
+<div id="Container">
+    <div id="header">
+        標題部分（Hear）
+    </div>
+    <div id="main">主体部分（main）</div>
+    <div id="footer">底部（footer）</div>
+</div>
+</body>
+
+</html>
